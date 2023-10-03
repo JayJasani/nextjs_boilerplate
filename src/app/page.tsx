ddf000b38135883065adb199cbf4c3253e83b26a
Jay Jasani <jasanijay8@gmail.com>
2023-10-03 11:33:17 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,7 @@
 import Head from 'next/head';
 
 import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from '~/svg/Logo.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
 
 export default function HomePage() {
   return (
@@ -25,24 +12,16 @@ export default function HomePage() {
         <title>Hi</title>
       </Head>
       <section className='bg-white'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
+        <div className='layout relative flex h-screen  flex-col items-center justify-center py-12 text-center'>
           <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
           <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
+            A starter for Next.js, Tailwind CSS, and TypeScript by Jay
           </p>
 
           <ButtonLink className='mt-6' href='/components' variant='light'>
             See all components
           </ButtonLink>
 
-          <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
-            </UnderlineLink>
-          </footer>
         </div>
       </section>
     </main>
